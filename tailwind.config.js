9d005ab1d15f4421771e31d28031325568e70566
Cheeto <kahod12450@gmail.com>
2021-11-25 18:41:40 +0800

    fix(global): windiCSS 配置檔修改

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ import formsPlugin from 'windicss/plugin/forms'
 
 export default defineConfig({
   darkMode: 'class',
-  safelist: 'p-3 p-4 p-5',
+  extract: {
+    include: ['src/**/*.{vue,html,jsx,tsx}'],
+    exclude: ['node_modules', '.git'],
+  },
   theme: {
     extend: {
       colors: {}
     }
   },
-  plugins: [formsPlugin],
+  /** 基礎表單樣式 */
+  plugins: [formsPlugin]
 })
