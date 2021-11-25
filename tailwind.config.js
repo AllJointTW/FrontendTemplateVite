2527556313e32754964cd98a90408e47f44a96a3
Cheeto <kahod12450@gmail.com>
2021-11-25 18:22:44 +0800

    feat(global): 增加 WindiCSS 設定檔

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'windicss/helpers'
+import formsPlugin from 'windicss/plugin/forms'
+
+export default defineConfig({
+  darkMode: 'class',
+  safelist: 'p-3 p-4 p-5',
+  theme: {
+    extend: {
+      colors: {}
+    }
+  },
+  plugins: [formsPlugin],
+})
